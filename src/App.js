a7fe9b1e46ed005cde0f3dcd7b7f8e22894ad5d5
Joshua Djiu <joshuadjiu78@gmail.com>
2025-10-16 16:25:02 +0700

    Add homepage and fix routing for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import PeerPartnerForm from "./pages/PeerPartnerForm";
 import CreativeTeamForm from "./pages/CreativeTeamForm";
 import StaffDashboard from "./pages/StaffDashboard";
